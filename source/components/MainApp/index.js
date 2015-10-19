a19d42dd9d0cec626f044bd895d89be297ff0ce2
Ramanpreet Nara <ramanpreetnara@gmail.com>
2015-10-18 22:00:41 -0400

    Verifying dependency injection works!

--- source/components/MainApp/index.js
+++ source/components/MainApp/index.js
@@ -3,4 +3,10 @@ import template from './template.html';
 export default class MainApp {
   static selector = 'main-app';
   static template = template;
+  constructor($log) {
+    this.$log = $log.log;
+  }
+  log() {
+    this.$log('dependency injection works!');
+  }
 }
